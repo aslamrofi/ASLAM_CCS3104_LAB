86ecc967eaf318cd8cb8af809988c659f2247de9
dawoodnadeem <captaindawood12@gmail.com>
2025-12-22 15:28:11 +0800

    new data

--- JavaFXLauncher.java
+++ JavaFXLauncher.java
@@ -32,7 +32,7 @@ public class JavaFXLauncher extends Application {
     static {
         loadLeaderboard();
     }
-
+//hsigiuh2eu
     static class PlayerStats implements Serializable {
         private static final long serialVersionUID = 1L;
         String playerName;
